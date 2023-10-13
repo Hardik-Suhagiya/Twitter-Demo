b8a4b97947965f3955d409faceea799d65327333
HARDIK SUHAGIYA <hardik4404@gmail.com>
2023-10-13 19:39:58 +0530

    - Add segment controller

--- Twitter-Demo/Modules/Home/View/Controller/HomeVC.swift
+++ Twitter-Demo/Modules/Home/View/Controller/HomeVC.swift
@@ -6,9 +6,11 @@
 //
 
 import UIKit
+import MXSegmentedControl
 
 class HomeVC: UIViewController {
     //MARK: - OUTLET
+    @IBOutlet weak var segmentControl: MXSegmentedControl!
     @IBOutlet weak var tblFeed: UITableView!
     
     //MARK: - PROPERTIES
@@ -24,15 +26,28 @@ class HomeVC: UIViewController {
 
 //MARK: - UI FUNCTION
 extension HomeVC {
-    private func initialSetUp() {
+    func initialSetUp() {
         setUpNavigationBar()
+        setupSegment()
         configureTableView()
     }
     
-    private func setUpNavigationBar() {
+    func setUpNavigationBar() {
         setupTitleImage(image: UIImage(named:"ic_logo")!)
-        setupLeftBarItem(image: UIImage(named:"ic_profile")!, completion: {()-> Void in
-            
-        })
+        setupLeftBarItem(image: UIImage(named:"ic_profile")!, completion: {()-> Void in })
+    }
+    
+    func setupSegment() {
+        segmentControl.font = .boldSystemFont(ofSize: 15)
+        segmentControl.append(title: "For you")
+        segmentControl.append(title: "Following")
+        segmentControl.addTarget(self, action: #selector(changeIndex(segmentedControl:)), for: .valueChanged)
+    }
+}
+
+//MARK: - ACTIONS
+extension HomeVC {
+    @objc func changeIndex(segmentedControl: MXSegmentedControl) {
+        debugPrint("Segment \(segmentedControl.selectedIndex) is selected")
     }
 }
